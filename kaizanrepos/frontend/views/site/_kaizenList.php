d11ad87fe457f426af79fc980b0eef85e92cdf69
Nishant Goel <nishant94goel@gmail.com>
2015-12-05 14:18:13 +0530

    Added files

--- kaizanrepos/frontend/views/site/_kaizenList.php
+++ kaizanrepos/frontend/views/site/_kaizenList.php
@@ -9,7 +9,7 @@
     <div class = "inner border-none clearfix">
         <div class = "photo"><img src = "assets/images/item-1.jpg"></div>
         <div class = "text">
-            <h4>A paragraph of title.</h4>
+            <h4><?= $model->subject; ?></h4>
             <p>A paragraph of text with on <a href = "#">unassigned link</a>.</p>
             <a href = "#">A second row of text with a web link</a>
         </div>
