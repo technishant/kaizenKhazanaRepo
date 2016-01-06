22d538c838155e8a7be1d7bc1dce97671f99f4df
prashant <ppgeu89@gmail.com>
2016-01-06 11:26:53 +0530

    //fix layout

--- kaizanrepos/backend/views/category/index.php
+++ kaizanrepos/backend/views/category/index.php
@@ -12,9 +12,9 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <!-- Content Header (Page header) -->
 <section class="content-header">
-    <h3>
-        <?= Html::encode($this->title) ?>
-    </h3>
+    <h1>
+<?= Html::encode($this->title) ?>
+    </h1>
 
 </section>
 
