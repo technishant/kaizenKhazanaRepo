af5b43940eb2ec2d15b9f9f68a3aa0e9eb9fad9d
Nishant <nishant94goel@gmail.com>
2017-01-07 14:56:30 +0530

    Update image fixes on capitalist

--- kaizanrepos/backend/controllers/CapitalistController.php
+++ kaizanrepos/backend/controllers/CapitalistController.php
@@ -13,125 +13,139 @@ use yii\web\UploadedFile;
 /**
  * CapitalistController implements the CRUD actions for Capitalist model.
  */
-class CapitalistController extends Controller
-{
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className()
-            ],
-        ];
-    }
+class CapitalistController extends Controller {
 
-    /**
-     * Lists all Capitalist models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new CapitalistSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+	public function behaviors() {
+		return [
+			'verbs' => [
+				'class' => VerbFilter::className()
+			],
+		];
+	}
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+	/**
+	 * Lists all Capitalist models.
+	 * @return mixed
+	 */
+	public function actionIndex() {
+		$searchModel = new CapitalistSearch();
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-    /**
-     * Displays a single Capitalist model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
+		return $this->render('index', [
+				'searchModel' => $searchModel,
+				'dataProvider' => $dataProvider,
+		]);
+	}
 
-    /**
-     * Creates a new Capitalist model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Capitalist();
+	/**
+	 * Displays a single Capitalist model.
+	 * @param integer $id
+	 * @return mixed
+	 */
+	public function actionView($id) {
+		return $this->render('view', [
+				'model' => $this->findModel($id),
+		]);
+	}
 
-        if ($model->load(Yii::$app->request->post())) {
-            $transaction = Yii::$app->db->beginTransaction();
-            try{
-                $uploadedFile = UploadedFile::getInstance($model, 'profile_photo');
-                if($uploadedFile) {
-                    $extensionName = $uploadedFile->extension;
-                    $fileName = time().'_'.uniqid().'.'.$extensionName;
-                    $model->profile_photo = $fileName;
-                }
-                if($model->validate() && $model->save()) {
-                    if($uploadedFile) {
-                        $uploadedFile->saveAs(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/'.$fileName);
-                    }
-                    $transaction->commit();
-                    return $this->redirect(['index']);
-                }
-            } catch (Exception $ex) {
-                $transaction->rollback();
-                @unlink(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/' . $fileName);
-            }
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
+	/**
+	 * Creates a new Capitalist model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 * @return mixed
+	 */
+	public function actionCreate() {
+		$model = new Capitalist();
 
-    /**
-     * Updates an existing Capitalist model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
+		if ($model->load(Yii::$app->request->post())) {
+			$transaction = Yii::$app->db->beginTransaction();
+			try {
+				$uploadedFile = UploadedFile::getInstance($model, 'profile_photo');
+				if ($uploadedFile) {
+					$extensionName = $uploadedFile->extension;
+					$fileName = time() . '_' . uniqid() . '.' . $extensionName;
+					$model->profile_photo = $fileName;
+				}
+				if ($model->validate() && $model->save()) {
+					if ($uploadedFile) {
+						$uploadedFile->saveAs(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/' . $fileName);
+					}
+					$transaction->commit();
+					return $this->redirect(['index']);
+				}
+			} catch (Exception $ex) {
+				$transaction->rollback();
+				@unlink(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/' . $fileName);
+			}
+		} else {
+			return $this->render('create', [
+					'model' => $model,
+			]);
+		}
+	}
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
+	/**
+	 * Updates an existing Capitalist model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id
+	 * @return mixed
+	 */
+	public function actionUpdate($id) {
+		$model = $this->findModel($id);
+		$prevProfilePhoto = $model->profile_photo;
+		if ($model->load(Yii::$app->request->post())) {
+			$transaction = Yii::$app->db->beginTransaction();
+			try {
+				$uploadedFile = UploadedFile::getInstance($model, 'profile_photo');
+				if ($uploadedFile) {
+					$extensionName = $uploadedFile->extension;
+					$fileName = time() . '_' . uniqid() . '.' . $extensionName;
+					$model->profile_photo = $fileName;
+				} else {
+					$model->profile_photo = $prevProfilePhoto;
+				}
+				if ($model->validate() && $model->save()) {
+					if ($uploadedFile) {
+						$uploadedFile->saveAs(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/' . $fileName);
+					}
+					$transaction->commit();
+					return $this->redirect(['index']);
+				}
+			} catch (Exception $ex) {
+				$transaction->rollback();
+				@unlink(Yii::getAlias('@frontend') . '/web/uploads/capitalist_images/' . $fileName);
+			}
+		} else {
+			return $this->render('update', [
+					'model' => $model,
+			]);
+		}
+	}
 
-    /**
-     * Deletes an existing Capitalist model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
+	/**
+	 * Deletes an existing Capitalist model.
+	 * If deletion is successful, the browser will be redirected to the 'index' page.
+	 * @param integer $id
+	 * @return mixed
+	 */
+	public function actionDelete($id) {
+		$this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
-    }
+		return $this->redirect(['index']);
+	}
 
-    /**
-     * Finds the Capitalist model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Capitalist the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Capitalist::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+	/**
+	 * Finds the Capitalist model based on its primary key value.
+	 * If the model is not found, a 404 HTTP exception will be thrown.
+	 * @param integer $id
+	 * @return Capitalist the loaded model
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	protected function findModel($id) {
+		if (($model = Capitalist::findOne($id)) !== null) {
+			return $model;
+		} else {
+			throw new NotFoundHttpException('The requested page does not exist.');
+		}
+	}
+
 }
